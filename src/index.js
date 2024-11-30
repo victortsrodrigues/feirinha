89cdb26d0edff8daed86afdae07751d5030edb32
victortsrodrigues <victadeu@gmail.com>
2024-11-30 15:26:52 -0300

    Post /items implementado considerando o retorno de status code conforme projeto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import express, { json } from "express";
 const app = express(); // cria o servirdor
 app.use(json()); // no POST o body vem como json e não como string
 
+const list = [];
 
+app.post("/items", (req, res) => {
+  const item = req.body;
+  console.log(item)
+  if(!item.name || !item.quantity || !item.type) {
+    return res.sendStatus(422);
+  }
+  if(list.find(elemento => elemento.name.toLowerCase() === item.name.toLowerCase())) {
+    return res.sendStatus(409);
+  }
+  list.push({
+    id: list.length + 1,
+    ...item
+  })
+  console.log(list)
+  res.sendStatus(201);
+})
 
 app.listen(5000, () => {
   console.log("Application running on server 5000");
