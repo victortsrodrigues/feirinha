1544c3a5c227fbdfefe03aefbebf3732f794e45a
victortsrodrigues <victadeu@gmail.com>
2024-11-30 15:54:11 -0300

    Implementos GETs e status code associados conforme projeto

--- src/index.js
+++ src/index.js
@@ -7,19 +7,40 @@ const list = [];
 
 app.post("/items", (req, res) => {
   const item = req.body;
-  console.log(item)
-  if(!item.name || !item.quantity || !item.type) {
+  if (!item.name || !item.quantity || !item.type) {
     return res.sendStatus(422);
   }
-  if(list.find(elemento => elemento.name.toLowerCase() === item.name.toLowerCase())) {
+  if (list.find(element => element.name.toLowerCase() === item.name.toLowerCase())) {
     return res.sendStatus(409);
   }
   list.push({
     id: list.length + 1,
     ...item
   })
-  console.log(list)
   res.sendStatus(201);
+})
+
+app.get("/items", (req, res) => {
+  const { type } = req.query;
+  if(type) {
+    const itemType = list.filter(element => {
+      return element.type.toLowerCase() === type.toLowerCase();
+    })
+    return res.send(itemType);
+  }
+  res.send(list);
+})
+
+app.get("/items/:id", (req, res) => {
+  const id = req.params.id;
+  if(Number(id) > 0) {
+    const itemId = list.find(element => element.id === Number(id))
+    if(!itemId) {
+      return res.sendStatus(404);
+    }
+    return res.send(itemId);
+  }
+  res.sendStatus(400);
 })
 
 app.listen(5000, () => {
